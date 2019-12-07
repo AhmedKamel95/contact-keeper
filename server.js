1622e06c90b29bdb23f2f1b8e72ebe185709fb60
ahmedkamel095 <ahmedkamel095@gmail.com>
2019-12-07 12:38:37 +0200

    Finished backend and initiated frontend

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+//Connect db
+connectDB();
+
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send({ msg: "Welcome to the contact keeper API" });
